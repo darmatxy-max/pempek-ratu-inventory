7643ed75dc08c4a16cadefabb84ea4d8313bd2c3
Darmawansyah <darmatxy@gmail.com>
2026-05-30 04:49:38 +0700

    Fix urutan useStoragePath untuk Laravel 12

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-// Trik Pamungkas: Alihkan storage ke /tmp dari inti bootstrap
+// Trik Pamungkas: Alihkan storage ke /tmp
 $appStoragePath = '/tmp/storage';
 
 if (!is_dir($appStoragePath)) {
@@ -16,8 +16,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->useStoragePath($appStoragePath) // <-- Letak yang benar, sebelum routing
+// 1. Buat aplikasinya terlebih dahulu
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -29,3 +29,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// 2. Override path SETELAH aplikasi berhasil dibuat
+$app->useStoragePath($appStoragePath);
+
+// 3. Kembalikan aplikasi yang sudah dimodifikasi
+return $app;
