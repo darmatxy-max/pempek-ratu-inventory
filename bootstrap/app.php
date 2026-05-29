c3e00b9082042d54e6deb54d77ab3ccc120b10fa
Darmawansyah <darmatxy@gmail.com>
2026-05-30 05:55:52 +0700

    Bypass ViewServiceProvider for serverless

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,22 +1,10 @@
 <?php
 
-// Trik Pamungkas: Alihkan storage ke /tmp
-$appStoragePath = '/tmp/storage';
-
-if (!is_dir($appStoragePath)) {
-    mkdir($appStoragePath, 0777, true);
-    mkdir($appStoragePath.'/framework/views', 0777, true);
-    mkdir($appStoragePath.'/framework/cache', 0777, true);
-    mkdir($appStoragePath.'/framework/cache/data', 0777, true);
-    mkdir($appStoragePath.'/framework/sessions', 0777, true);
-    mkdir($appStoragePath.'/logs', 0777, true);
-}
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-// 1. Buat aplikasinya terlebih dahulu
+// Buat aplikasi dengan konfigurasi minimal
 $app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -30,8 +18,7 @@ $app = Application::configure(basePath: dirname(__DIR__))
         //
     })->create();
 
-// 2. Override path SETELAH aplikasi berhasil dibuat
-$app->useStoragePath($appStoragePath);
+// PENTING: Paksa jalankan penyimpanan ke /tmp
+$app->useStoragePath('/tmp/storage');
 
-// 3. Kembalikan aplikasi yang sudah dimodifikasi
 return $app;
